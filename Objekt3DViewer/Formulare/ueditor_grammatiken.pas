d386bf16bd742f7c81e5a451cfcac8f2ac4757a9
Dark3y <321skrillex123@gmail.com>
2021-01-19 16:04:29 +0100

    änderungen uGrammatik
    Probleme: ein paar mini probleme mit den floats(copy + pos) und strings, wegen falschen pos

--- Objekt3DViewer/Formulare/ueditor_grammatiken.pas
+++ Objekt3DViewer/Formulare/ueditor_grammatiken.pas
@@ -6,7 +6,7 @@ interface
 
 uses
   Classes, SysUtils, Forms, Controls, Graphics, Dialogs, ExtCtrls, StdCtrls,
-  CheckLst, ComCtrls,uTurtle,uParameter;
+  CheckLst, ComCtrls,uTurtle;
 
 type
 
--- Objekt3DViewer/Formulare/ugrammatiken.pas
+++ Objekt3DViewer/Formulare/ugrammatiken.pas
@@ -12,6 +12,8 @@ type
 
   TuGrammatiken = class(TForm)
     Button1: TButton;
+    Button2: TButton;
+    Button3: TButton;
     Edit1: TEdit;
     Anzahl: TLabel;
     Edit2: TEdit;
@@ -25,6 +27,7 @@ type
     procedure AnzahlClick(Sender: TObject);
     procedure Button1Click(Sender: TObject);
     procedure Button2Click(Sender: TObject);
+    procedure Button3Click(Sender: TObject);
     procedure Edit1Change(Sender: TObject);
     procedure Edit2Change(Sender: TObject);
     procedure Edit3Change(Sender: TObject);
@@ -52,19 +55,27 @@ begin
 end;
 //zeichenstyle fehlt noch
 procedure TuGrammatiken.Button1Click(Sender: TObject);
-var i,n,anzahl:CARDINAL;MemoLine: TStringArray;
-    gram:TGrammatik;L,NameGrammatik,S:String;
+var i,n,anzahl:CARDINAL;
+    gram:TGrammatik;R,L,NameGrammatik:String;
+    Lc:Char;
+    W:REAL;
     Turtle:TTurtle;zeichenPara: TZeichenParameter;
+    p,s: Integer;
 begin
   gram:=TGrammatik.Create;
   n:=0;
-  S:=Memo1.Lines[0];
-  gram.axiom:= S[1];
-  While n-1>= Memo1.Lines.Count do
+  p:=pos('->',Memo1.Lines[0]);
+  gram.axiom:= copy(Memo1.Lines[0],1,p-1);
+  While n<= Memo1.Lines.Count do
     begin
-    L:=Memo1.Lines[n];
-    //wahrscheinlichkeit
-    gram.addRegel(L[1],L[4..100]);
+    p:=pos('->',Memo1.Lines[n]);
+    s:=pos(',',Memo1.Lines[n]);
+    L:=copy(Memo1.Lines[n],1,p-1);//linke Seite des '->'
+    Lc:=L[1]; //StrToChar
+    R:=copy(Memo1.Lines[n],1,p-1);//rechte Seite des '->'
+    delete(R,1,s-2);              //,aber hinter dem Komma
+    W:=strtofloat(copy(Memo1.Lines[n],1,s-2));//wahrscheinlichkeit
+    gram.addRegel(Lc,R,W);
     INC(n);
     end;
   //
@@ -80,12 +91,55 @@ begin
        Turtle.name:=NameGrammatik;
        Hauptform.update_startkoords();
        Hauptform.o.addTurtle(Turtle);
+       Hauptform.zeichnen();
   end;
   Visible:=False;
 end;
 
 procedure TuGrammatiken.Button2Click(Sender: TObject);
+var turtle: TTurtle;
+    datei: String;
 begin
+  turtle.speichern(datei);
+end;
+
+procedure TuGrammatiken.Button3Click(Sender: TObject);
+var i,n,anzahl:CARDINAL;MemoLine: TStringArray;
+    gram:TGrammatik;L,R,NameGrammatik:String;
+    W:REAL;
+    zeichenPara: TZeichenParameter;
+    p,s:Integer;
+    Lc:Char;
+    turtle: TTurtle;
+    datei: String;
+    begin
+      gram:=TGrammatik.Create;
+      n:=0;
+      p:=pos('->',Memo1.Lines[0]);
+      gram.axiom:= copy(Memo1.Lines[0],1,p-1);
+      While n-1>= Memo1.Lines.Count do
+        begin
+        p:=pos('->',Memo1.Lines[n]);
+        s:=pos(',',Memo1.Lines[n]);
+        L:=copy(Memo1.Lines[n],1,p-1);//linke Seite des ->
+        Lc:=L[1];
+        R:=copy(Memo1.Lines[n],1,p-2);//rechte Seite des ->
+        delete(R,1,s-2); //,aber hinter dem Komma
+        W:=strtofloat(copy(Memo1.Lines[n],1,s-2));//wahrscheinlichkeit
+        gram.addRegel(Lc,R,W);
+        INC(n);
+        end;
+  //
+  zeichenPara.rekursionsTiefe:= strtoint(Edit2.Text);
+  zeichenPara.winkel:=strtofloat(Edit3.Text);
+  NameGrammatik:=Edit4.Text;
+  anzahl:= strtoint(Edit1.Text)-1;
+   //speichern des Turtels
+  for i:=0 to anzahl do
+  begin
+       zeichenPara.setzeStartPunkt(Hauptform.akt_x,Hauptform.akt_y,Hauptform.akt_z);
+       turtle:= TTurtle.Create(datei);
+  end;
 end;
 
 procedure TuGrammatiken.Edit1Change(Sender: TObject);

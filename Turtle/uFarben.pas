6fc0800fa4e8f138af861127224041adda4a92fe
mike0609king <mike0609king@users.noreply.github.com>
2021-03-25 14:04:51 +0100

    Farben hinzugefuegt.

--- Turtle/uFarben.pas
+++ Turtle/uFarben.pas
@@ -12,7 +12,7 @@ end;
 
 type TFarben = record
   private
-    FIdxZuFarbe: array[1..15] of TFarbe;
+    FIdxZuFarbe: array[1..25] of TFarbe;
   public
     procedure initColor;
     function gibFarbe(idx: Cardinal) : TFarbe;
@@ -37,6 +37,20 @@ begin
   FIdxZuFarbe[13].r := 1; FIdxZuFarbe[13].g := 0.8; FIdxZuFarbe[13].b := 0;
   FIdxZuFarbe[14].r := 1; FIdxZuFarbe[14].g := 1; FIdxZuFarbe[14].b := 1;
   FIdxZuFarbe[15].r := 0; FIdxZuFarbe[15].g := 1; FIdxZuFarbe[15].b := 0;
+
+  //autumn palette
+  FIdxZuFarbe[16].r := 0.933; FIdxZuFarbe[16].g := 0.69; FIdxZuFarbe[16].b := 0.388;
+  FIdxZuFarbe[17].r := 0.949; FIdxZuFarbe[17].g := 0.855; FIdxZuFarbe[17].b := 0.58;
+  FIdxZuFarbe[18].r := 0.886; FIdxZuFarbe[18].g := 0.553; FIdxZuFarbe[18].b := 0.471;
+  FIdxZuFarbe[19].r := 0.78; FIdxZuFarbe[19].g := 0.271; FIdxZuFarbe[19].b := 0.208;
+  FIdxZuFarbe[20].r := 0.549; FIdxZuFarbe[20].g := 0.165; FIdxZuFarbe[20].b := 0.153;
+
+  //sakura palette
+  FIdxZuFarbe[21].r := 0.337; FIdxZuFarbe[21].g := 0.129; FIdxZuFarbe[21].b := 0.208;
+  FIdxZuFarbe[22].r := 0.765; FIdxZuFarbe[22].g:= 0.51; FIdxZuFarbe[22].b := 0.62;
+  FIdxZuFarbe[23].r := 0.914; FIdxZuFarbe[23].g := 0.694; FIdxZuFarbe[23].b := 0.804;
+  FIdxZuFarbe[24].r := 1; FIdxZuFarbe[24].g := 0.906; FIdxZuFarbe[24].b := 0.871;
+  FIdxZuFarbe[25].r := 0.988; FIdxZuFarbe[25].g := 0.82; FIdxZuFarbe[25].b := 0.843;
 end;
 
 function TFarben.gibFarbe(idx: Cardinal) : TFarbe;
